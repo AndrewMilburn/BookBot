45edf994f8f421ddd970867aea487d6c05f86f90
Andrew Milburn <sidbell.069@gmail.com>
2025-08-12 08:51:47 +0100

    Error Message corrected, more books added

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     if len(sys.argv) > 1:
         path_to_file = sys.argv[1]
     else:
-        print("Usage: python3 main.py <path_to_book ")
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 #    path_to_file = "books/frankenstein.txt"
     book = get_book_text(path_to_file)
